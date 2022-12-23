11b340b5522cdc3599c13ddf2b4793049ec7705d
Taranbeer Singh <taran.ts.singh@gmail.com>
2022-12-23 12:04:52 -0600

    Array

--- Array/main.go
+++ Array/main.go
@@ -3,7 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var arr [4]int
+	var arr [5]int
+	arr[0] = 10
+	arr[1] = 20
+	arr[2] = 30
+	arr[3] = 40
+	arr[4] = 50
+
+	ar := [5]int{2, 4, 6, 8, 10}
 
 	fmt.Println(arr)
+	fmt.Printf("%T", (arr))
+	fmt.Println(ar)
+
 }
